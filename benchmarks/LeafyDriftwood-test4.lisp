a9b0858ea4809c975003a1a46ba45ea9bc34d7f4
Manav Chakravarthy <manavchakravarthy@mac.mynetworksettings.com>
2025-12-02 23:42:01 -0500

    updated format

--- benchmarks/LeafyDriftwood-test4.lisp
+++ benchmarks/LeafyDriftwood-test4.lisp
@@ -1,17 +1,6 @@
+(define (sum-acc n total)
+  (if (= n 0)
+      total
+      (sum-acc (sub1 n) (+ total n))))
 
-function not(x) = 
-  if x 
-  then false
-  else true
-
-function is_even(x) =
-  if x = 0
-  then true
-  else is_odd(sub1(x))
-
-function is_odd(x) =
-  if x = 0
-  then false
-  else not(is_even(x))
-
-print(is_even(10))
+(print (sum-acc 5 0))
